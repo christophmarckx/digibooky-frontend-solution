32d0df5048f6140ac0d4dc4b25ca7bd13eeff8a8
vercruyssetim <tim.vercruysse@cegeka.be>
2023-01-11 12:03:01 +0100

    improved update book feature

--- src/app/layout/update-book/update-book.component.ts
+++ src/app/layout/update-book/update-book.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Book} from "../../model/Book";
 import {BookService} from "../../serviceBook/book.service";
-import {PRIMARY_OUTLET, Router} from "@angular/router";
+import {ActivatedRoute, PRIMARY_OUTLET, Router} from "@angular/router";
 import {NonNullableFormBuilder} from "@angular/forms";
 import {LibrarianService} from "../../serviceLibrarian/librarian.service";
+import {mergeMap} from "rxjs";
 
 @Component({
   selector: 'app-update-book',
@@ -12,66 +13,65 @@ import {LibrarianService} from "../../serviceLibrarian/librarian.service";
 })
 export class UpdateBookComponent implements OnInit {
   private _bookForm = this.formBuilder.group({
-    isbn: '',
-    title: '',
-    authorFirstname: '',
-    authorLastname: '',
-    author: '',
-    copies: 0,
-    price: 0,
-    arrival: '',
-    image: '',
-    dueDate: '',
-    lenderNames: [],
-    lendingId: ''
+      isbn: '',
+      title: '',
+      authorFirstname: '',
+      authorLastname: '',
+      author: '',
+      copies: 0,
+      price: 0,
+      arrival: '',
+      image: '',
+      dueDate: '',
+      lenderNames: [],
+      lendingId: ''
     }
   );
-  private isbn!: string;
-  public book!: Book;
-  public errors!: Array<string>
+  public errors: string[] = [];
 
-  constructor(private formBuilder: NonNullableFormBuilder, private librarianService: LibrarianService, private bookService: BookService, private route: Router) {
+  constructor(private formBuilder: NonNullableFormBuilder,
+              private librarianService: LibrarianService,
+              private bookService: BookService,
+              private route: ActivatedRoute,
+              private router: Router
+  ) {
   }
 
   ngOnInit(): void {
-    this.isbn = this.route.parseUrl(this.route.url).root.children[PRIMARY_OUTLET].segments[1].path;
-    this.bookService.getBook(this.isbn).subscribe(book => {
-      this.book = book
+    this.route.paramMap.pipe(
+      mergeMap(params => this.bookService.getBook(params.get('isbn')!)),
+    ).subscribe(book => {
       this._bookForm.patchValue(book)
     });
-    this.errors = [];
   }
 
   onSubmit(bookvalues: any): void {
     // Process checkout data here
-    this.errors = [];
-    this.hasError(this._bookForm.getRawValue());
-    if (this.errors.length == 0) {
-      this.bookService.updateBook(bookvalues).subscribe();
-      console.warn('Book was updated.', this._bookForm.value);
-      this.route.navigate(["/books/" + this.book.isbn]).then(() => window.location.reload())
+    this.errors = this.getError(this._bookForm.getRawValue());
+    if (this.errors.length != 0) {
+      return;
     }
+    this.bookService.updateBook(bookvalues)
+      .pipe(
+        mergeMap(() => this.router.navigate(["/books/" + this._bookForm.value.isbn]))
+      ).subscribe();
   }
 
-  hasError(book: Book) {
+  getError(book: Book) {
+    let errors = [];
     if (book.title == "") {
-      this.errors.push("Title is not filled in");
+      errors.push("Title is not filled in");
     }
     if (book.authorFirstname == "") {
-      this.errors.push("Authors firstname is not filled in");
+      errors.push("Authors firstname is not filled in");
     }
     if (book.authorLastname == "") {
-      this.errors.push("Authors lastname is not filled in");
+      errors.push("Authors lastname is not filled in");
     }
     if (book.price == 0) {
-      this.errors.push("price is not filled in");
+      errors.push("price is not filled in");
     }
-    if (book.copies == 0) {
-      book.copies = 1;
-    }
-    if (book.image == "") {
-      book.image = "";
-    }
+    return errors;
   }
 
   get bookForm() {
