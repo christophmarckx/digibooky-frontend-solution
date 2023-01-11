c9884aee092e8a24461e75a3a7add5a504396554
vercruyssetim <tim.vercruysse@cegeka.be>
2023-01-11 14:24:08 +0100

    improved add book layout

--- src/app/layout/register/register-book/register-book.component.ts
+++ src/app/layout/register/register-book/register-book.component.ts
@@ -4,7 +4,7 @@ import {Router} from "@angular/router";
 import {BookService} from "../../../serviceBook/book.service";
 import {Book} from "../../../model/Book";
 import {LibrarianService} from "../../../serviceLibrarian/librarian.service";
-import {catchError} from "rxjs";
+import {catchError, mergeMap} from "rxjs";
 
 @Component({
   selector: 'app-register-book',
@@ -18,22 +18,22 @@ export class RegisterBookComponent implements OnInit {
     authorFirstname: '',
     authorLastname: '',
     author: '',
-    copies: 0,
+    copies: 1,
     price: 0,
     arrival: '',
-    image: '',
+    image: 'https://archive.switchfully.com/track/functional/digibooky/assets/book.jpg',
     dueDate: '',
     lenderNames: [],
     lendingId: ''
   });
 
-  private books: Array<Book>;
   public errors: Array<string>;
+  public open = false;
 
-  constructor(private formBuilder: NonNullableFormBuilder, private librarianService: LibrarianService, private bookService: BookService, private route: Router) {
-    this.formBuilder = formBuilder;
-    this.books = [];
-    this.bookService.books.subscribe(books => this.books = books);
+  constructor(private formBuilder: NonNullableFormBuilder,
+              private librarianService: LibrarianService,
+              private bookService: BookService,
+              private route: Router) {
     this.errors = [];
   }
 
@@ -41,48 +41,47 @@ export class RegisterBookComponent implements OnInit {
   }
 
   onSubmit(bookvalues: any): void {
-    // Process checkout data here
-    console.log(bookvalues)
-    this.errors = [];
-    this.hasError(this._bookForm.getRawValue());
-    if (this.errors.length == 0) {
-      this.bookService.addBook(bookvalues)
-        .pipe(
-          catchError(error => {
-            this.errors.push(error.error.message);
-            return error;
-          })
-        )
-        .subscribe(() => this.route.navigate(["/books"]).then(() => window.location.reload()));
+    this.errors = this.getErrors(this._bookForm.getRawValue());
+    if (this.errors.length != 0) {
+      return;
     }
+
+    this.bookService.addBook(bookvalues)
+      .pipe(
+        catchError(error => {
+          this.errors.push(error.error.message);
+          return error;
+        }),
+        mergeMap(() => this.route.navigate(["/books"]))
+      )
+      .subscribe();
   }
 
-  hasError(book: Book) {
-    // Check for all the required, but empty fields
+  getErrors(book: Book) {
+    let errors: string[] = []
     if (book.isbn == "") {
-      this.errors.push("ISBN is not filled in");
+      errors.push("ISBN is not filled in");
     }
     if (book.title == "") {
-      this.errors.push("Title is not filled in");
+      errors.push("Title is not filled in");
     }
     if (book.authorFirstname == "") {
-      this.errors.push("Authors firstname is not filled in");
+      errors.push("Authors firstname is not filled in");
     }
     if (book.authorLastname == "") {
-      this.errors.push("Authors lastname is not filled in");
+      errors.push("Authors lastname is not filled in");
     }
     if (book.price == 0) {
-      this.errors.push("price is not filled in");
+      errors.push("price is not filled in");
     }
-    if (book.copies == 0) {
-      book.copies = 1;
-    }
-    if (book.image == "") {
-      book.image = "";
-    }
+    return errors;
   }
 
   get bookForm(): FormGroup {
     return this._bookForm;
+  }
+
+  toggle() {
+    this.open = !this.open;
   }
 }
